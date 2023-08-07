b87063eedaf50e586bf2720bbd7001a79046ba59
ksrky <xgrk9475@gmail.com>
2023-08-07 16:49:54 +0900

    remove secd related files

--- bin/main.ml
+++ bin/main.ml
@@ -1,9 +1,1 @@
-let rec repl () =
-  print_string "# ";
-  let inp = read_line () in
-  if inp = "#quit" then ()
-  else (
-    (try CamlKit.Interpreter.eval inp with CamlKit.ErrorMsg.Error -> ());
-    repl () )
-
-let () = repl ()
+let () = ()
