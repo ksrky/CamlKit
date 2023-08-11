597f4538a9fdea6922c6fabf3cb9bfaa111c21ae
ksrky <xgrk9475@gmail.com>
2023-08-11 20:34:22 +0900

    reformat examples

--- lib/runtime.c
+++ lib/runtime.c
@@ -1,12 +1,12 @@
 #include "stdio.h"
 
 extern long writec(long x) {
-        putchar((char)x);
-        return 0;
+    putchar((char)x);
+    return 0;
 }
 
 extern long readc() {
-        long x;
-        scanf("%ld", &x);
-        return x;
+    long x;
+    scanf("%ld", &x);
+    return x;
 }
