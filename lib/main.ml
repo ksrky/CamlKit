e3ad29c5790464dbd1680ff1b7791bca59907bfb
ksrky <xgrk9475@gmail.com>
2023-08-15 10:58:59 +0900

    update main.ml

--- lib/main.ml
+++ lib/main.ml
@@ -1,26 +1,32 @@
+(** [run path] evaluates a source file on the virtual machine. *)
 let run (path : string) =
-  let abs = Parse.parse path in
-  (* print_endline (AbsSyn.pretty_exp abs); *)
-  let sexp = Semant.trans_exp Env.empty abs in
-  let instrs = Compile.compile sexp in
+  let abssyn = Parse.parse path in
+  (* print_endline (AbsSyn.ppr_exp abssyn); *)
+  let intsyn = Semant.trans_exp Env.empty abssyn in
+  (* print_endline (IntSyn.ppr_exp intsyn); *)
+  let instrs = Compile.compile intsyn in
   (* print_endline (Machine.show_instrs instrs); *)
   Stack.init (); Machine.load_instrs instrs; Machine.run_commands ()
 
+(** [eval inp] evaluates string [inp] on the virtual machine. *)
 let eval (inp : string) =
-  let abs = Parse.parse_line inp in
-  (* print_endline (AbsSyn.pretty_exp abs); *)
-  let sexp = Semant.trans_exp Env.empty abs in
-  let instrs = Compile.compile sexp in
+  let abssyn = Parse.parse_line inp in
+  (* print_endline (AbsSyn.ppr_exp abssyn); *)
+  let intsyn = Semant.trans_exp Env.empty abssyn in
+  (* print_endline (IntSyn.ppr_exp intsyn); *)
+  let instrs = Compile.compile intsyn in
   (* print_endline (Machine.show_instrs instrs); *)
   Stack.init (); Machine.load_instrs instrs; Machine.run_commands ()
 
+(** [compile path] compiles a source file to LLVM IR and output to a .ll file. *)
 let compile (path : string) : unit =
-  let abs = Parse.parse path in
-  (* print_endline (AbsSyn.pretty_exp abs); *)
-  let intsyn = Semant.trans_exp Env.empty abs in
+  let abssyn = Parse.parse path in
+  (* print_endline (AbsSyn.ppr_exp abssyn); *)
+  let intsyn = Semant.trans_exp Env.empty abssyn in
+  (* print_endline (IntSyn.ppr_exp intsyn); *)
   let intsyn' = ClosConv.f intsyn in
   let defs = Lifting.f intsyn' in
-  (*print_endline (IntSyn.ppr_defs defs*)
+  (* print_endline (IntSyn.ppr_defs defs); *)
   LlvmGen.codegen_builtins ();
   List.iter (fun def -> Llvm.dump_value (LlvmGen.codegen_func def)) defs;
   Llvm.print_module (Filename.remove_extension path ^ ".ll") LlvmGen.the_module
