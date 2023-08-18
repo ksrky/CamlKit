d9d3ae20f5aa61fed8945ed6251fe31d2de1df95
ksrky <xgrk9475@gmail.com>
2023-08-18 11:23:26 +0900

    use gather func once in contraction steps

--- lib/contraction.ml
+++ lib/contraction.ml
@@ -11,20 +11,27 @@ let thresh_ratio : float = 0.95 (* tmp *)
 let nred = ref 1
 
 type varinfo =
-  {mutable _side_effect: bool; mutable usecount: int; mutable repres: I.exp; mutable simple: bool}
+  { mutable _side_effect: bool
+  ; mutable usecount: int
+  ; mutable repres: I.exp
+  ; mutable simple: bool
+  ; mutable isrec: bool }
 
-let default_info () = {_side_effect= true; usecount= 0; repres= I.Nil; simple= false}
+let default_info () = {_side_effect= true; usecount= 0; repres= I.Nil; simple= false; isrec= false}
 
-let hashtable : (int, varinfo) Hashtbl.t = Hashtbl.create ~random:true 4096
+let hashtbl : (int, varinfo) Hashtbl.t = Hashtbl.create ~random:true 4096
 
 let hashtbl_find (id : Ident.t) : varinfo =
-  try Hashtbl.find hashtable (Ident.unique id)
-  with Not_found ->
-    let info = default_info () in
-    Hashtbl.add hashtable (Ident.unique id) info;
-    info
+  try Hashtbl.find hashtbl (Ident.unique id)
+  with Not_found -> ErrorMsg.impossible "Unknown identifier at hashtbl_find"
 
-let init_usecount ids : unit = List.iter (fun id -> (hashtbl_find id).usecount <- 0) ids
+let init_varinfo ?(isrec = false) ids : unit =
+  List.iter
+    (fun id ->
+      let info = default_info () in
+      info.isrec <- isrec;
+      Hashtbl.add hashtbl (Ident.unique id) info )
+    ids
 
 let incr_usecount id : unit =
   let info = hashtbl_find id in
@@ -42,24 +49,23 @@ let update_repres id exp =
   match exp with Int _ | Nil | Var _ -> info.simple <- true | _ -> info.simple <- false
 
 let find_repres (id : Ident.t) : I.exp =
-  match Hashtbl.find_opt hashtable (Ident.unique id) with
+  match Hashtbl.find_opt hashtbl (Ident.unique id) with
   | Some info -> (* inlining *) incr nred; decr_usecount id; info.repres
   | None -> Var id
-
-let init () = nred := 1
 
 let rec gather : I.exp -> unit = function
   | Var id -> incr_usecount id
   | App (Lam (vars, body), args) ->
       (* (fun x1 x2 .. xn -> e) a1 a2 .. ak *)
+      init_varinfo vars;
       (try List.iter2 update_repres vars args with Invalid_argument _ (* n > k *) -> ());
-      init_usecount vars; gather body; List.iter gather args
+      gather body; List.iter gather args
   | App (fcn, args) -> gather fcn; List.iter gather args
-  | Lam (vars, body) -> init_usecount vars; gather body
+  | Lam (vars, body) -> init_varinfo vars; gather body
   | Builtin (_, args) -> List.iter gather args
-  | Let (_, vars, bnds, body) ->
+  | Let (isrec, vars, bnds, body) ->
+      if isrec then init_varinfo ~isrec:true vars else init_varinfo vars;
       List.iter2 update_repres vars bnds;
-      init_usecount vars;
       List.iter gather bnds;
       gather body
   | If (test, then_, else_) -> gather test; gather then_; gather else_
@@ -80,6 +86,7 @@ let rec reduce : I.exp -> I.exp = function
                else (v, reduce a) :: acc )
              vars args [] )
       in
+      (try List.iter2 update_repres vars' args' with Invalid_argument _ (* n > k *) -> ());
       if vars' = [] then reduce body else App (Lam (vars', reduce body), args')
   | App (fcn, args) -> App (reduce fcn, List.map reduce args)
   | Lam (vars, body) -> Lam (vars, reduce body)
@@ -95,10 +102,13 @@ let rec reduce : I.exp -> I.exp = function
                else (v, reduce b) :: acc )
              vars bnds [] )
       in
+      List.iter2 update_repres vars' bnds';
       if vars' = [] then reduce body else Let (isrec, vars', bnds', reduce body)
   | e -> e
 
-let step exp : I.exp = init (); gather exp; reduce exp
+let step exp : I.exp =
+  nred := 1;
+  reduce exp
 
 let rec steps (n : int) (exp : I.exp) =
   if n = 0 then exp
@@ -107,3 +117,5 @@ let rec steps (n : int) (exp : I.exp) =
     let exp' = step exp in
     let next = float_of_int !nred /. float_of_int prev > thresh_ratio in
     if next then steps (n - 1) exp' else exp'
+
+let steps n exp = gather exp; steps n exp
