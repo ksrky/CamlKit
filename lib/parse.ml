5ad6b0d816321b4555cff4635609e3869e9a4ab7
ksrky <xgrk9475@gmail.com>
2023-09-10 23:57:48 +0900

    fix merge conflicts

--- lib/parse.ml
+++ lib/parse.ml
@@ -1,9 +1,9 @@
-let parse (filename : string) : AbsSyn.def list =
+let parse (filename : string) : AbsSyn.exp =
   let file = open_in filename in
   let inp = really_input_string file (in_channel_length file) in
   let linebuf = Lexing.from_string inp in
-  try Parser.prog Lexer.token linebuf with _ -> ErrorMsg.error "parse error"; []
+  try Parser.prog Lexer.token linebuf with _ -> ErrorMsg.error "parse error"; AbsSyn.NilExp
 
 let parse_line (inp : string) : AbsSyn.exp =
   let linebuf = Lexing.from_string inp in
-  try Parser.line Lexer.token linebuf with _ -> ErrorMsg.error "parse error"; AbsSyn.NilExp
+  try Parser.prog Lexer.token linebuf with _ -> ErrorMsg.error "parse error"; AbsSyn.NilExp
--- lib/scoping.mli
+++ lib/scoping.mli
@@ -5,5 +5,4 @@ val initial : scope
 
 val get_reservedid : string -> Ident.t
 val scoping_exp : scope -> AbsSyn.exp -> AbsSyn.exp
-val scoping_bnds : scope -> AbsSyn.bnd list -> AbsSyn.bnd list
-val scoping_defs : scope -> AbsSyn.def list -> AbsSyn.def list
+val scoping_bnds : scope -> AbsSyn.bnd list -> AbsSyn.bnd list 
