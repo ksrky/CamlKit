fd987b1209af5219a1b85bdb33aeeba556c85256
ksrk <xgrk9475@gmail.com>
2023-10-04 13:53:28 +0900

    tmp lifting is not type-preserving

--- lib/lifting.ml
+++ lib/lifting.ml
@@ -1,50 +1,52 @@
 open IntSyn
 
-let remove : 'a list -> 'a list -> 'a list = List.fold_right (fun x -> List.filter (( <> ) x))
-
-let rec free_vars : exp -> binders = function
-  | Int _ -> []
-  | Nil -> []
-  | Var var -> [var]
-  | App (fcn, args) -> free_vars fcn @ List.concat_map free_vars args
-  | Lam (bndrs, body) -> remove (free_vars body) bndrs
-  | Prim (_, args) -> List.concat_map free_vars args
-  | Let (false, bndrs, exps, body) -> List.concat_map free_vars exps @ remove (free_vars body) bndrs
-  | Let (true, bndrs, exps, body) -> remove (List.concat_map free_vars exps @ free_vars body) bndrs
-  | If (test, then', else') -> free_vars test @ free_vars then' @ free_vars else'
-  | Seq (exp, rest) -> free_vars exp @ free_vars rest
-
 let frags : frags ref = ref []
 
 let append (frag : frag) = frags := frag :: !frags
 
-let rec lift_lam (exp : exp) : exp =
+let rec lift_lam (exp : exp) : binders * exp =
   match exp with
-  | Int _ -> exp
-  | Nil -> exp
-  | Var _ -> exp
-  | App (fcn, args) -> App (lift_lam fcn, List.map lift_lam args)
+  | Int _ -> ([], exp)
+  | Nil -> ([], exp)
+  | Var var -> ([var], exp)
+  | App (fcn, args) ->
+      let vars1, fcn' = lift_lam fcn in
+      let vars2, args' = lift_lams args in
+      (vars1 @ vars2, App (fcn', args'))
   | Lam (vars, body) ->
       let id = Ident.fresh () in
-      let fvs = remove (free_vars body) vars in
-      append {name= Ident.unique_name id; params= fvs @ vars; body= lift_lam body};
-      App (Var (id, TypeCheck.type_of [] exp), List.map (fun fv -> Var fv) fvs)
-  | Prim (fcn, args) -> Prim (fcn, List.map lift_lam args)
+      let vars', body' = lift_lam body in
+      let fvs = List.fold_right (fun x -> List.filter (( <> ) x)) vars' vars in
+      append {name= Ident.unique_name id; params= fvs @ vars; body= body'};
+      ([], App (Var (id, TypeCheck.type_of [] exp), List.map (fun fv -> Var fv) fvs))
+  | Prim (fcn, args) ->
+      let vars, args' = lift_lams args in
+      (vars, Prim (fcn, args'))
   | Let (_, vars, exps, body) ->
-      let memo = ref [] in
       List.iter2
-        (fun (id, ty) -> function
-          | Lam (params, exp) -> append {name= Ident.unique_name id; params; body= lift_lam exp}
-          | exp -> memo := ((id, ty), exp) :: !memo )
+        (fun (id, _) -> function
+          | Lam (vars1, exp) ->
+              let vars2, exp' = lift_lam exp in
+              append {name= Ident.unique_name id; params= vars2 @ vars1; body= exp'}
+          | exp ->
+              let vars1, exp' = lift_lam exp in
+              append {name= Ident.unique_name id; params= vars1; body= exp'} )
         vars exps;
-      if !memo = [] then lift_lam body
-      else
-        let f = Ident.fresh () and params = List.map fst !memo in
-        append {name= Ident.unique_name f; params; body= lift_lam body};
-        lift_lam (App (Var (f, TypeCheck.type_of [] (Lam (params, body))), List.map snd !memo))
-  | If (test, then', else') -> If (lift_lam test, lift_lam then', lift_lam else')
-  | Seq (exp, rest) -> Seq (lift_lam exp, lift_lam rest)
+      lift_lam body
+  | If (test, then_, else_) ->
+      let vars1, test' = lift_lam test in
+      let vars2, then' = lift_lam then_ in
+      let vars3, else' = lift_lam else_ in
+      (vars1 @ vars2 @ vars3, If (test', then', else'))
+  | Seq (exp, rest) ->
+      let vars1, exp' = lift_lam exp in
+      let vars2, rest' = lift_lam rest in
+      (vars1 @ vars2, Seq (exp', rest'))
+
+and lift_lams (exps : exp list) : binders * exp list =
+  let varss, exps' = List.split (List.map lift_lam exps) in
+  (List.concat varss, exps')
 
 let f (exp : exp) : frag list =
-  let ans = lift_lam exp in
+  let _, ans = lift_lam exp in
   List.rev ({name= "main"; params= []; body= ans} :: !frags)
