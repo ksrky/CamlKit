811ac6b1651886b26ad6eae88f9d7de44888df0f
ksrk <xgrk9475@gmail.com>
2023-10-29 14:22:29 +0900

    fix lifting and create llvm dir

--- mono/lifting/lifting.ml
+++ mono/lifting/lifting.ml
@@ -1,54 +1,55 @@
-module C = Core.Syntax
-
 let notin : 'a list -> 'a list -> 'a list = List.fold_right (fun x -> List.filter (( <> ) x))
 
 let frags : Syntax.frags ref = ref []
 
 let append (frag : Syntax.frag) = frags := frag :: !frags
 
-let rec lift_lam (exp : C.exp) : C.id list * C.exp =
+let rec lift_lam (exp : Syntax.exp) : Syntax.id list * Syntax.exp =
   match exp with
   | Var var -> ([var], exp)
   | Int _ -> ([], exp)
   | Nil -> ([], exp)
   | App {fcn; args} ->
-      let vars1, fcn' = lift_lam fcn in
-      let vars2, args' = lift_lams args in
-      (vars1 @ vars2, App {fcn= fcn'; args= args'})
+      let fvs1, fcn' = lift_lam fcn in
+      let fvs2, args' = lift_lams args in
+      (fvs1 @ fvs2, App {fcn= fcn'; args= args'})
   | Lam {vars; body} ->
-      let id = Id.fresh () in
+      let tmp = Id.fresh () in
       let vars', body' = lift_lam body in
       let fvs = notin vars' vars in
-      append {name= Id.unique_name id; params= fvs @ vars; body= body'};
-      ([], App {fcn= Var id; args= List.map (fun fv -> C.Var fv) fvs})
+      append {name= Id.unique_name tmp; params= fvs @ vars; body= body'};
+      (fvs, App {fcn= Var tmp; args= List.map (fun fv -> Syntax.Var fv) fvs})
   | Prim {oper; args} ->
-      let vars, args' = lift_lams args in
-      (vars, Prim {oper; args= args'})
-  | Let {vars; bnds; body; _} ->
-      List.iter2
-        (fun var -> function
-          | C.Lam {vars= ps1; body= exp} ->
-              let ps2, exp' = lift_lam exp in
-              let fvs = notin ps2 ps1 in
-              let tmp = Id.fresh () in
-              if fvs = [] then append {name= Id.unique_name var; params= ps1; body= exp'}
-              else (
-                append {name= Id.unique_name tmp; params= fvs @ ps1; body= exp'};
-                append
-                  { name= Id.unique_name var
-                  ; params= []
-                  ; body= C.App {fcn= C.Var tmp; args= List.map (fun fv -> C.Var fv) fvs} } )
-          | exp ->
-              let params, exp' = lift_lam exp in
-              append {name= Id.unique_name var; params; body= exp'} )
-        vars bnds;
-      lift_lam body
-  | _ -> failwith "not implemented"
+      let fvs, args' = lift_lams args in
+      (fvs, Prim {oper; args= args'})
+  | Let {isrec; vars; bnds; body} ->
+      let fvs_bnds, bnds' =
+        List.split
+          (List.map
+             (fun exp ->
+               let ps1, exp' =
+                 match exp with Syntax.Lam {vars= ps1; body= exp} -> (ps1, exp) | _ -> ([], exp)
+               in
+               let ps2, exp' = lift_lam exp in
+               let fvs = notin ps2 (vars @ ps1) in
+               let tmp = Id.fresh () in
+               append {name= Id.unique_name tmp; params= fvs @ ps1; body= exp'};
+               (fvs, Syntax.App {fcn= Syntax.Var tmp; args= List.map (fun fv -> Syntax.Var fv) fvs})
+               )
+             bnds )
+      in
+      let fvs_body, body' = lift_lam body in
+      (List.concat fvs_bnds @ fvs_body, Let {isrec; vars; bnds= bnds'; body= body'})
+  | If {cond; then_; else_} ->
+      let fvs1, cond' = lift_lam cond in
+      let fvs2, then' = lift_lam then_ in
+      let fvs3, else' = lift_lam else_ in
+      (fvs1 @ fvs2 @ fvs3, If {cond= cond'; then_= then'; else_= else'})
 
-and lift_lams (exps : C.exp list) : C.id list * C.exp list =
+and lift_lams (exps : Syntax.exp list) : Syntax.id list * Syntax.exp list =
   let varss, exps' = List.split (List.map lift_lam exps) in
   (List.concat varss, exps')
 
-let f (exp : C.exp) : Syntax.frag list =
+let f (exp : Syntax.exp) : Syntax.frag list =
   lift_lam exp |> ignore;
   List.rev !frags
