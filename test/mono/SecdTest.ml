d2146135f90a29e7ca6b161fc51654c4fc7e1171
ksrky <xgrk9475@gmail.com>
2023-11-14 11:59:52 +0900

    test: add secdTest

---
+++ test/mono/SecdTest.ml
@@ -0,0 +1,43 @@
+open Mono
+
+(* simple integer *)
+let%expect_test _ = Main.eval "42"; [%expect {| 42 |}]
+
+(* binary operator *)
+let%expect_test _ = Main.eval "3 - 7"; [%expect {| -4 |}]
+
+(* if expression *)
+let%expect_test _ =
+  Main.eval "if true then 10 else -10";
+  [%expect {| 10 |}]
+
+(* lambda abstraction and application *)
+let%expect_test _ =
+  Main.eval "(fun x -> x + 1) 2";
+  [%expect {| 3 |}]
+
+(* let expression with mutiple arguments *)
+let%expect_test _ =
+  Main.eval "let mod x y = x - y * (x / y) in mod 10 3";
+  [%expect {| 1 |}]
+
+(* higher order function *)
+let%expect_test _ =
+  Main.eval "let twice f x = f (f x) in twice (fun x -> x + 1) 0";
+  [%expect {| 2 |}]
+
+(* recursive function *)
+let%expect_test _ =
+  Main.eval "let rec fact n = if n = 0 then 1 else n * fact (n - 1) in fact 5";
+  [%expect {| 120 |}]
+
+(* recursive function with multiple arguments *)
+let%expect_test _ =
+  Main.eval
+    "let rec gcd x y = if y = 0 then x else gcd y (x - y * (x / y)) in gcd 8 12";
+  [%expect {| 4 |}]
+
+(* nested function definitions *)
+let%expect_test _ =
+  Main.eval "let f x = let g y = let h z = x + y + z in h 3 in g 2 in f 1";
+  [%expect {| 6 |}]
--- test/mono/parseTest.ml
+++ test/mono/parseTest.ml
@@ -3,15 +3,20 @@ open Language.Syntax
 
 let%test _ = Parse.parse_line "42" = IntExp 42
 
-let%test _ = Parse.parse_line "3 - 7" = OpExp {left= IntExp 3; op= MinusOp; right= IntExp 7}
+let%test _ =
+  Parse.parse_line "3 - 7" = OpExp {left= IntExp 3; op= MinusOp; right= IntExp 7}
 
-let%test _ = Language.Syntax.ppr_exp (Parse.parse_line "fun x y -> x + y") = "fun x y -> x + y"
+let%test _ = ppr_exp (Parse.parse_line "fun x y -> x + y") = "fun x y -> x + y"
 
 let%test _ =
   Parse.parse_line "if true then 0 else 1"
   = IfExp {cond= BoolExp true; then_= IntExp 0; else_= IntExp 1}
 
 let%test _ =
-  Language.Syntax.ppr_exp (Parse.parse_line "let f x y = y in f 1") = "let f x y = y in f 1"
+  ppr_exp (Parse.parse_line "let f x y = y in f 1") = "let f x y = y in f 1"
 
 let%test _ = Language.Syntax.ppr_exp (Parse.parse_line "- f x") = "0 - f x"
+
+let%test _ =
+  ppr_exp (Parse.parse_line "let rec o x = e (x-1) and e x = o (x-1) in o 11")
+  = "let rec o x = e (x - 1) and e x = o (x - 1) in o 11"
