0129003952643387cfa598e431e311fa48a08fda
ksrk <xgrk9475@gmail.com>
2023-12-15 12:13:15 +0900

    add types to closure conversion

--- mono/cps/closConv.ml
+++ mono/cps/closConv.ml
@@ -9,13 +9,13 @@ type locals = id list
 let remove_dup xs =
   List.fold_right (fun x xs -> if List.mem x xs then xs else x :: xs) xs []
 
-let ( // ) init elims : 'a list =
-  List.fold_right (fun x -> List.filter (( <> ) x)) elims init
+let ( // ) escs lcls : 'a list =
+  List.fold_right (fun x -> List.filter (fun (y, _) -> x <> y)) lcls escs
 
-let lookup_env (escs : escapes) (x : id) : int * escapes =
+let lookup_env (escs : escapes) (x : var) : int * escapes =
   let rec find_idx i = function
     | [] -> (i, escs @ [x])
-    | y :: ys -> if x = y then (i, escs) else find_idx (i + 1) ys
+    | y :: ys -> if fst x = fst y then (i, escs) else find_idx (i + 1) ys
   in
   find_idx 1 escs
 
@@ -28,35 +28,37 @@ let globals : id list ref = ref []
 let rec cc_val (escs : escapes) (lcls : locals) : value -> value * escapes =
   function
   | Const c -> (Const c, escs)
-  | Var x when List.mem x lcls -> (Var x, escs)
-  | Var f when List.mem f !globals -> (Glb f, escs)
+  | Var x when List.mem (fst x) lcls -> (Var x, escs)
+  | Var f when List.mem (fst f) !globals -> (Glb f, escs)
   | Var x ->
       let _, escs' = lookup_env escs x in
       (Var x, escs')
   | Glb _ -> failwith "unreahcble"
   | Lam {vars; body} ->
-      let body', escs' = cc_exp [] vars body in
-      let env_var = Id.from_string "env" in
-      let env_ty = failwith "not implemented" in
+      let body', escs' = cc_exp [] (List.map fst vars) body in
+      let env_var = (Id.from_string "env", TupleTy (List.map snd vars)) in
       if escs' = [] then (
+        let var = (Id.from_string "func", ContTy (List.map snd vars)) in
         append_fundef
-          { var= Id.from_string "func"
-          ; params= vars
-          ; body= mk_let (mk_projs (Var env_var, env_ty) escs') body' };
-        (Glb name, escs) )
-      else (
+          {var; params= vars; body= mk_let (mk_projs (Var env_var) escs') body'};
+        (Glb var, escs) )
+      else
+        let var =
+          (Id.from_string "func", ContTy (List.map snd (env_var :: vars)))
+        in
         append_fundef
-          { name
-          ; vars= env_var :: vars
+          { var
+          ; params= env_var :: vars
           ; body= mk_let (mk_projs (Var env_var) escs') body' };
-        ( Tuple [Glb name; Tuple (mk_vars escs')]
-        , remove_dup (escs @ escs') // lcls ) )
-  | Tuple _ -> failwith "not implemented"
+        ( Tuple [Glb var; Tuple (mk_vars escs')]
+        , remove_dup (escs @ escs') // lcls )
+  | Tuple vals ->
+      let vals', escs' = cc_val_seq escs lcls vals in
+      (Tuple vals', escs')
 
 and cc_val_seq (escs : escapes) (lcls : locals) (vals : value list) :
     value list * escapes =
-  let loop (acc : value list) (escs : escapes) :
-      value list -> value list * escapes = function
+  let loop acc escs = function
     | [] -> (List.rev acc, escs)
     | val_ :: vals ->
         let val', escs' = cc_val escs lcls val_ in
@@ -66,18 +68,18 @@ and cc_val_seq (escs : escapes) (lcls : locals) (vals : value list) :
 
 and cc_exp (escs : escapes) (lcls : locals) : exp -> exp * escapes = function
   | Let {dec; body} ->
-      let dec', escs1, name = cc_dec escs lcls dec in
-      let body', escs2 = cc_exp escs1 (name :: lcls) body in
+      let dec', escs1, lcls' = cc_dec escs lcls dec in
+      let body', escs2 = cc_exp escs1 lcls' body in
       (Let {dec= dec'; body= body'}, escs2)
   | Letrec {fundefs; body} ->
-      let names = List.map (fun {name} -> name) fundefs in
-      let cc_fundef ({name; vars; body} : fundef) : escapes =
-        let env_var = Id.from_string "env" in
-        globals := names;
-        let body', escs' = cc_exp [] vars body in
+      let glbs = List.map (fun {var} -> var) fundefs in
+      let cc_fundef ({var; params; body} : fundef) : escapes =
+        globals := List.map fst glbs;
+        let body', escs' = cc_exp [] (List.map fst params) body in
+        let env_var = (Id.from_string "env", TupleTy (List.map snd params)) in
         append_fundef
-          { name
-          ; vars= env_var :: vars
+          { var
+          ; params= env_var :: params
           ; body= mk_let (mk_projs (Var env_var) escs') body' };
         escs'
       in
@@ -91,11 +93,13 @@ and cc_exp (escs : escapes) (lcls : locals) : exp -> exp * escapes = function
       match fcn' with
       (* tmp: closures are always tuple *)
       | Tuple _ ->
-          let clos_var = Id.from_string "clos" in
-          let code_var = Id.from_string "code" in
-          let env_var = Id.from_string "env" in
+          let env_var = (Id.from_string "env", TupleTy (List.map snd escs2)) in
+          let code_var = (Id.from_string "code", ContTy [snd env_var]) in
+          let clos_var =
+            (Id.from_string "clos", TupleTy [snd code_var; snd env_var])
+          in
           ( mk_let
-              ( ValDec {name= clos_var; val_= fcn'}
+              ( ValDec {var= clos_var; val_= fcn'}
               :: mk_projs (Var clos_var) [code_var; env_var] )
               (App {fcn= Var code_var; args= Var env_var :: args'})
           , escs2 )
@@ -109,15 +113,15 @@ and cc_exp (escs : escapes) (lcls : locals) : exp -> exp * escapes = function
       let val', escs' = cc_val escs lcls val_ in
       (Halt val', escs')
 
-and cc_dec (escs : escapes) (lcls : locals) : dec -> dec * escapes * id =
+and cc_dec (escs : escapes) (lcls : locals) : dec -> dec * escapes * locals =
   function
-  | ValDec {name; val_} ->
+  | ValDec {var; val_} ->
       let val', escs' = cc_val escs lcls val_ in
-      (ValDec {name; val_= val'}, escs', name)
-  | PrimDec {name; left; oper; right} ->
+      (ValDec {var; val_= val'}, escs', fst var :: lcls)
+  | PrimDec {var; left; oper; right} ->
       let left', escs1 = cc_val escs lcls left in
       let right', escs2 = cc_val escs1 lcls right in
-      (PrimDec {name; left= left'; oper; right= right'}, escs2, name)
+      (PrimDec {var; left= left'; oper; right= right'}, escs2, fst var :: lcls)
   | ProjDec _ -> failwith "not implemented"
 
 let cc_prog (exp : exp) : fundef list * exp =
